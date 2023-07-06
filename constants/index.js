58a649af54ef98021e2d97af6102ec5491b046c1
rajitha-00 <rajithatw07102@gmail.com>
2023-07-06 18:19:01 +0530

    icons done

--- constants/index.js
+++ constants/index.js
@@ -34,9 +34,9 @@ export const exploreWorlds = [
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
+  'Junior Analyst - HCLTech (Sri Lanka) [ 2021/October - Present ]',
+  'Frontend Developer - Infini Solutions (Sri Lanka) [ 2021/April - 2021/November ]',
+  'Freelance - Raptor Solutions/ Loons Lab (Sri Lanka) [ 2023 ]',
 ];
 
 export const newFeatures = [
--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -25,13 +25,15 @@ const GetStarted = () => (
           alt="get-started"
           className="w-[90%] h-[90%] object-contain"
         />
+       
       </motion.div>
+     
       <motion.div
         variants={fadeIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
         <TypingText title="| How I Work Here" textStyles="text-center text-orange-400"/>
-        <TitleText title={<>Get started with just a few clicks</>} />
+        <TitleText title={<>Career Experience</>} />
         <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
           {startingFeatures.map((feature, index) => (
             <StartSteps
